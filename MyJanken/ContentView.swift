2c891b654e6ba690104baa139194f4362e56e785
tommy <tmtmtoa@gmail.com>
2023-05-30 23:27:17 +0900

    グー画像指定

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -4,13 +4,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        // グー画像を指定
+        Image("gu")
+            .resizable()
+            .scaledToFit()
     }
 }
 
