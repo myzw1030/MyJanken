9a2735b57e9f5b6ee16dcf6ba08af5791eaa15a7
tommy <tmtmtoa@gmail.com>
2023-06-14 22:28:57 +0900

    buttonメソッド

--- MyJanken/ContentView.swift
+++ MyJanken/ContentView.swift
@@ -4,10 +4,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        // グー画像を指定
-        Image("gu")
-            .resizable()
-            .scaledToFit()
+        VStack {
+            // グー画像を指定
+            Image("gu")
+                .resizable()
+                .scaledToFit()
+            // じゃんけんの種類を指定
+            Text("グー")
+            
+            Button {
+                print("タップされたよ！")
+            } label: {
+                Text("じゃんけんをする")
+            }
+
+        }
+        
     }
 }
 
